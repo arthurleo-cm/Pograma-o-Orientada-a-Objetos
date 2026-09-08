5fd4d67945ed3ed6d3ecef09dce9b77ee28ca4b3
Arthur <arthurhleocadio@gmail.com>
2026-09-08 19:46:53 -0300

    Update questao2.cpp

--- LISTA_DE_EXERCICIO/questao2.cpp
+++ LISTA_DE_EXERCICIO/questao2.cpp
@@ -19,3 +19,145 @@ requisitos apresentados. Obrigatoriamente incorporar construtor, polimorfismo,
 métodos get e set, e sobreposição de classe: toString [retornar: nome
 funcionário, salário bruto, salário líquido e enquadramento funcional] .
 */
+#include <string>
+#include <iostream>
+using namespace std;
+
+class FUNCIONARIO
+{
+private:
+    string nome;
+    float salarioHora, horasTrabalhadas;
+    int filhos, enquadramento;
+    float salarioBruto, adicionalFilhos, descontoIR, salarioLiquido;
+
+public:
+    FUNCIONARIO()
+    {
+        nome = "";
+        salarioHora = 0;
+        horasTrabalhadas = 0;
+        filhos = 0;
+        enquadramento = 0;
+        salarioBruto = 0;
+        adicionalFilhos = 0;
+        descontoIR = 0;
+        salarioLiquido = 0;
+    };
+
+    void setValores(string n, float sh, float ht, int f, int e)
+    {
+        nome = n;
+        salarioHora = sh;
+        horasTrabalhadas = ht;
+        filhos = f;
+        enquadramento = e;
+    };
+
+    void calcular()
+    {
+        salarioBruto = salarioHora * horasTrabalhadas;
+
+        float percentualFilhos = filhos * 5;
+
+        if (percentualFilhos > 20)
+        {
+            percentualFilhos = 20;
+        }
+
+        adicionalFilhos = salarioBruto * percentualFilhos / 100;
+
+        float totalBruto = salarioBruto + adicionalFilhos;
+
+        if (totalBruto < 4000)
+        {
+            descontoIR = 0;
+        }
+        else if (totalBruto < 8000)
+        {
+            descontoIR = totalBruto * 15 / 100;
+        }
+        else if (totalBruto < 12000)
+        {
+            descontoIR = totalBruto * 22.5 / 100;
+        }
+        else
+        {
+            descontoIR = totalBruto * 27.5 / 100;
+        }
+
+        salarioLiquido = totalBruto - descontoIR;
+    };
+
+    string getNome()
+    {
+        return nome;
+    }
+
+    float getSalarioBruto()
+    {
+        return salarioBruto;
+    }
+
+    float getSalarioLiquido()
+    {
+        return salarioLiquido;
+    }
+
+    int getEnquadramento()
+    {
+        return enquadramento;
+    }
+
+    string toString()
+    {
+        return nome;
+    }
+};
+
+int main()
+{
+    string nome;
+    float salarioHora, horas;
+    int filhos, enquadramento;
+
+    FUNCIONARIO funcionario;
+
+    printf("\nCADASTRO DE FUNCIONARIO");
+
+    printf("\nDigite o nome: ");
+    getline(cin, nome);
+
+    printf("\nDigite o salario por hora: ");
+    scanf("%f", &salarioHora);
+
+    printf("\nDigite as horas trabalhadas: ");
+    scanf("%f", &horas);
+
+    printf("\nDigite o numero de filhos: ");
+    scanf("%d", &filhos);
+
+    printf("\nDigite o enquadramento funcional (0 a 4): ");
+    scanf("%d", &enquadramento);
+
+    funcionario.setValores(
+        nome,
+        salarioHora,
+        horas,
+        filhos,
+        enquadramento
+    );
+
+    funcionario.calcular();
+
+    printf("\n\nRESULTADO");
+    printf("\nNome: %s", funcionario.getNome().c_str());
+    printf("\nSalario bruto: R$ %7.2f",
+        funcionario.getSalarioBruto());
+    printf("\nSalario liquido: R$ %7.2f",
+        funcionario.getSalarioLiquido());
+    printf("\nEnquadramento funcional: %d",
+        funcionario.getEnquadramento());
+
+    return 0;
+}
